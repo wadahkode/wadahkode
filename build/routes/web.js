8de3aba018aba89efc1014a8ee0900888706cb1d
wadahkode <mvp.dedefilaras@gmail.com>
2021-01-06 20:25:03 +0700

    update

--- build/routes/web.js
+++ build/routes/web.js
@@ -1,9 +1,11 @@
 var Route = require('../router');
 var auth = require('../authenticated');
+require('jsdom-global')();
 auth();
 Route.group('/', function () {
     Route.get('index.html', function () {
-        console.log('Hello');
+        var container = document.getElementById('root');
+        container.innerHTML = 'Selamat datang di wadahgamer';
     });
     Route.get('login', 'login');
 });
