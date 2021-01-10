3a53057bedbdf84aefed6883ce77f2225bbd2986
wadahkode <mvp.dedefilaras@gmail.com>
2021-01-10 21:57:00 +0700

    update

---
+++ app.js
@@ -0,0 +1,19 @@
+const wadahkode = require('@wadahkode/server'),
+  app = wadahkode(),
+  path = require('path'),
+  port = process.env.PORT || 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  console.log(
+    '🌏 %s %s %s %s',
+    req.method,
+    res.statusCode,
+    new Date(),
+    req.url
+  );
+  
+  res.render('index', {title: 'Welcome | Wadahgamer'});
+});
+app.listen(port);
--- src/routes/web.ts
+++
@@ -1,8 +0,0 @@
-const Route = require('../router');
-const welcome = require('../controller/welcome')();
-
-Route.group('/', function(){
-  Route.get('index.html', () => welcome.index());
-});
-
-Route.group('/home', function(){});
