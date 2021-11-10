c4d52c261095105a2e2809c9d4a51ff1f541c763
wadahkode <mvp.dedefilaras@gmail.com>
2021-11-10 11:02:55 +0700

    fix view on desktop and mobile, and update data tutorial by newest and populer

--- docs/dist/js/wadahkode.js
+++ docs/dist/js/wadahkode.js
@@ -1,6 +1,7 @@
-const articles = document.querySelector("#newest");
-const url = "https://api-ngeblog.herokuapp.com";
-// const url = "http://localhost/ngeblog/";
+const newest = document.querySelector("#newest");
+const populer = document.querySelector("#populer");
+// const url = "https://api-ngeblog.herokuapp.com";
+const url = "http://localhost/ngeblog/";
 
 let myHeaders = new Headers();
 myHeaders.append("Accept", "application/json");
@@ -8,12 +9,12 @@ myHeaders.append("Accept", "application/json");
 const templateArticles = (data, timestamps) => `
   <article>
     <picture>
-      <img src="${data.foto}" alt="..."/>
+      <img src="${data.foto}" alt=""/>
     </picture>
     <div>
       <div class="article-header">
         <h2>${data.title}</h2>
-        <sub>${timestamps}</sub>
+        <time datetime="${timestamps}">${timestamps}</time>
       </div>
 
       <div class="article-body">
@@ -23,8 +24,25 @@ const templateArticles = (data, timestamps) => `
   </article>
 `;
 
-async function getArticles() {
-  const response = await fetch(url).then((response) => response);
+async function getArticles(keywords = null) {
+  let response = undefined;
+
+  switch (keywords) {
+    case "newest":
+      response = await fetch(url + "?artikel=newest").then(
+        (response) => response
+      );
+      break;
+
+    case "populer":
+      response = await fetch(url + "?artikel=populer").then(
+        (response) => response
+      );
+      break;
+
+    default:
+      response = await fetch(url).then((response) => response);
+  }
 
   return response.json();
 }
@@ -36,12 +54,25 @@ getArticles().then((response) => {
     if (today.getTime() === new Date(item.createdAt).getTime()) {
       console.log("artikel terbaru");
     } else {
-      articles.innerHTML += templateArticles(
+      newest.innerHTML += templateArticles(
         item,
         today.getTime() === new Date(item.updatedAt).getTime()
           ? item.updatedAt
           : item.createdAt
       );
     }
+  });
+});
+
+getArticles("populer").then((response) => {
+  const today = new Date();
+
+  response.map((item) => {
+    populer.innerHTML += templateArticles(
+      item,
+      today.getTime() === new Date(item.updatedAt).getTime()
+        ? item.updatedAt
+        : item.createdAt
+    );
   });
 });
